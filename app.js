b0bdb6ba9f8e7cc636383a4a7683982acf174d27
snafkin92 <87398136+snafkin92@users.noreply.github.com>
2021-07-19 11:44:40 +0900

    Add files via upload
    modify answer comment

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const quiz = [
   {
     question: '第１問：サザエさんに登場するマスオさんの同僚である穴子さんの唇の厚さは何センチでしょうか？',
     answers: [ '① 6センチ', '② 4.7センチ', '③ 13.5センチ'],
-    correct: '③ 13.5センチ'
+    correct: '③ 13.5センチ',
+    explanation: '\n穴子さんの唇の厚さは、約13.5センチと言われています。かなり分厚いですよね。ちなみに、年齢は27歳だそうです。'
   }, {
     question: '第２問：たい焼きになる前は、何の形で売られていたでしょうか？',
     answers: [ '① 亀', '② カニ', '③ 金魚'],
-    correct: '① 亀'
+    correct: '① 亀',
+    explanation: '\n最初は、鯛ではなく亀の形で売り出したのですが全く売れなかった為、高級魚である鯛の形に変えられました。'
   }, {
     question: '第３問：一目惚れをしやすい年齢は何歳でしょうか？',
     answers: [ '① 5歳', '② 14歳', '③ 23歳'],
-    correct: '② 14歳'
+    correct: '② 14歳',
+    explanation: '\n14歳の時は、恋愛ホルモンが最も活発に活動するからだそうです。'
   }
 ];
 
@@ -47,16 +50,16 @@ const goToNext = () => {
 
 const judge = (elm) => {
   if(elm.textContent === quiz[quizCount].correct){
-    $window.alert('正解です');
+    $window.alert('正解です。' + quiz[quizCount].explanation);
     score++;
   } else {
-    $window.alert('不正解です');
+    $window.alert('不正解です。');
   }
   goToNext();
 };
 
 const showEnd = () => {
-  $question.textContent = 'あなたのスコアは' + score + '/' + quizLen + 'です';
+  $question.textContent = 'あなたのスコアは' + score + '/' + quizLen + 'でした。';
   
   const $items = $doc.getElementById('js-items');
   $items.style.visibility = 'hidden';
